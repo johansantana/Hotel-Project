7222af177b647115ce48396cd32df3dd6ada410e
Johan Santana <johancarlosst@gmail.com>
2024-03-01 12:38:55 -0400

    feat: api controllers and minor refactoring to repository

---
+++ Hotel.Api/Controllers/RolUsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hotel.Infrastructure;
+using Hotel.Api.Models;
+
+namespace Hotel.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RolUsuarioController : ControllerBase
+{
+    private readonly IRolUsuarioRepository rolUsuarioRepository;
+    public RolUsuarioController(IRolUsuarioRepository rolUsuarioRepository)
+    {
+        this.rolUsuarioRepository = rolUsuarioRepository;
+    }
+    // GET: api/<RolUsuarioController>
+    [HttpGet("GetRolUsuario")]
+    public IActionResult Get()
+    {
+        var rolUsuario = this.rolUsuarioRepository.GetEntities();
+        return Ok(rolUsuario);
+    }
+
+    // GET api/<RolUsuarioController>/5
+    [HttpGet("GetRolUsuarioById")]
+    public IActionResult Get(int id)
+    {
+        var rolUsuario = this.rolUsuarioRepository.GetEntity(id);
+        return Ok(rolUsuario);
+    }
+
+    // POST api/<UsuarioController>
+    [HttpPost("AddRolUsuario")]
+    public void Post([FromBody] RolUsuarioAddModel rolUsuarioModel)
+    {
+        this.rolUsuarioRepository.Add(new RolUsuario()
+        {
+            Descripcion = rolUsuarioModel.Descripcion,
+            Estado = rolUsuarioModel.Estado
+        });
+    }
+
+    // PUT api/<RolUsuarioController>/5
+    [HttpPut("UpdateRolUsuario")]
+    public void Put(int id, [FromBody] RolUsuarioAddModel rolUsuarioModel)
+    {
+        var rolUsuarioToUpdate = rolUsuarioRepository.GetEntity(id) ?? throw new RolUsuarioException("Rol de Usuario no encontrado");
+        rolUsuarioToUpdate.Descripcion = rolUsuarioModel.Descripcion;
+        rolUsuarioToUpdate.Estado = rolUsuarioModel.Estado;
+        this.rolUsuarioRepository.Update(rolUsuarioToUpdate);
+    }
+
+    // DELETE api/<UsuarioController>/5
+    [HttpDelete("DeleteUsuario")]
+    public void Delete(int id)
+    {
+        var rolUsuarioDeleted = rolUsuarioRepository.GetEntity(id) ?? throw new RolUsuarioException("Rol de Usuario no encontrado");
+        this.rolUsuarioRepository.Remove(rolUsuarioDeleted);
+    }
+}
---
+++ Hotel.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hotel.Infrastructure;
+using Hotel.Api.Models;
+
+namespace Hotel.Api.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsuarioController : ControllerBase
+{
+    private readonly IUsuarioRepository usuarioRepository;
+    public UsuarioController(IUsuarioRepository usuarioRepository)
+    {
+        this.usuarioRepository = usuarioRepository;
+    }
+    // GET: api/<UsuarioController>
+    [HttpGet("GetUsuario")]
+    public IActionResult Get()
+    {
+        var usuario = this.usuarioRepository.GetEntities();
+        return Ok(usuario);
+    }
+
+    // GET api/<UsuarioController>/5
+    [HttpGet("GetUsuarioById")]
+    public IActionResult Get(int id)
+    {
+        var usuario = this.usuarioRepository.GetEntity(id);
+        return Ok(usuario);
+    }
+
+    // POST api/<UsuarioController>
+    [HttpPost("AddUsuario")]
+    public void Post([FromBody] UsuarioAddModel usuarioModel)
+    {
+        this.usuarioRepository.Add(new Usuario()
+        {
+            NombreCompleto = usuarioModel.NombreCompleto,
+            Correo = usuarioModel.Correo,
+            Clave = usuarioModel.Clave,
+            Estado = usuarioModel.Estado,
+            IdRolUsuario = usuarioModel.IdRolUsuario
+        });
+    }
+
+    // PUT api/<UsuarioController>/5
+    [HttpPut("UpdateUsuario")]
+    public void Put(int id, [FromBody] UsuarioAddModel usuarioModel)
+    {
+        var usuarioToUpdate = usuarioRepository.GetEntity(id) ?? throw new UsuarioException("Usuario no encontrado");
+        usuarioToUpdate.Clave = usuarioModel.Clave;
+        usuarioToUpdate.Correo = usuarioModel.Correo;
+        usuarioToUpdate.NombreCompleto = usuarioModel.NombreCompleto;
+        usuarioToUpdate.Estado = usuarioModel.Estado;
+        usuarioToUpdate.IdRolUsuario = usuarioModel.IdRolUsuario;
+        this.usuarioRepository.Update(usuarioToUpdate);
+    }
+
+    // DELETE api/<UsuarioController>/5
+    [HttpDelete("DeleteUsuario")]
+    public void Delete(int id)
+    {
+        var usuarioDeleted = usuarioRepository.GetEntity(id) ?? throw new UsuarioException("Usuario no encontrado");
+        this.usuarioRepository.Remove(usuarioDeleted);
+    }
+}
+
