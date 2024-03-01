1353d97f200d81b2e915a1b4c25ff1f2c127eb4c
Johan Santana <johancarlosst@gmail.com>
2024-03-01 16:27:57 -0400

    feat: register controller and context

--- Hotel.Api/Program.cs
+++ Hotel.Api/Program.cs
@@ -1,6 +1,19 @@
+using Hotel.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Connections.
+builder.Services.AddDbContext<HotelContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HotelContext")));
+
+// Repositories.
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+// App Services.
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,7 +36,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- Hotel.Infrastructure/Context/HotelContext.cs
+++ Hotel.Infrastructure/Context/HotelContext.cs
@@ -10,9 +10,4 @@ public class HotelContext : DbContext
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<RolUsuario> RolUsuarios { get; set; }
     public DbSet<Categoria> Categoria { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Hotel_DB");
-    }
 }
