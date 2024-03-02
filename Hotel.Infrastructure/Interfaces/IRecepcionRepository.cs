9c6cc7b892d36f650983632a182b40d58002a79b
Adorcito <158768805+Adorcito@users.noreply.github.com>
2024-03-02 17:53:19 -0400

    Update IRecepcionRepository.cs

--- Hotel.Infrastructure/Interfaces/IRecepcionRepository.cs
+++ Hotel.Infrastructure/Interfaces/IRecepcionRepository.cs
@@ -2,11 +2,5 @@ namespace Hotel.Infrastructure.Interfaces
 {
     public interface IRecepcionRepository : IBaseRepository<Recepcion>
     {
-        IEnumerable<Recepcion> GetRecepciones();
-        Recepcion? GetRecepcion(int idRecepcion);
-        void SaveRecepcion(Recepcion recepcion);
-        void RemoveRecepcion(Recepcion recepcion);
-        void UpdateRecepcion(Recepcion recepcion);
-        IEnumerable<Recepcion> FindRecepcionesByCriteria(Func<Recepcion, bool> criteria);
     }
 }
