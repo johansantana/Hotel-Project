fd8bb302f59383a4f4067a6b29f1b7a8ada30803
Adorcito <158768805+Adorcito@users.noreply.github.com>
2024-03-02 00:02:49 -0400

    Update Recepcion.cs

--- Hotel.Domain/Entities/Recepcion.cs
+++ Hotel.Domain/Entities/Recepcion.cs
@@ -1,6 +1,17 @@
 namespace Hotel.Domain;
 public class Categoria : BaseEntity
 {
-    public string Recepcionista { get; set; }
-    public DateTime FechaRecepcion { get; set; }
+   public int Id { get; set; }
+    public DateTime FechaDeEntrada { get; set; }
+    public DateTime? FechaDeSalida { get; set; }
+    public string Estado { get; set; } // Ejemplo: "Libre", "Ocupado", "Reservado"
+    public int NumeroDeHabitacion { get; set; }
+    public string NombreDelHuesped { get; set; }
+    public string ApellidoDelHuesped { get; set; }
+    public string DocumentoIdentificacion { get; set; }
+    public void CheckIn()
+    {
+    }
+
+    public void CheckOut()
 }
