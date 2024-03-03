08597dc872b5c2c87f0d811065869893f784267e
Ador <adorsantiago003@gmail.com>
2024-03-02 20:03:29 -0400

    añade los cambios

---
+++ Hotel.Infrastructure/Interfaces/IRecepcionRepository.cs
@@ -0,0 +1,10 @@
+using Hotel.Domain.Repository;
+using Hotel.Domain.Entities;
+
+namespace Hotel.Infrastructure.Interfaces
+{
+    public interface IRecepcionRepository : IBaseRepository<Recepcion>
+    {
+        
+    }
+}
