af6f422cce1381cefe6f6f3be84bbd38cf913bbd
Johan Santana <johancarlosst@gmail.com>
2024-03-09 19:56:37 -0400

    chore: connection string and inf proj

--- Hotel.Infrastructure/Context/HotelContext.cs
+++ Hotel.Infrastructure/Context/HotelContext.cs
@@ -7,4 +7,13 @@ public class HotelContext : DbContext
     // Cargar aqui los modelos
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<RolUsuario> RolUsuarios { get; set; }
+
+    public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=JOHAN\\SQLEXPRESS;Initial Catalog=DBHotel;Integrated Security=True;User ID=sa;Password=200520; TrustServerCertificate=true;");
+    }
 }
