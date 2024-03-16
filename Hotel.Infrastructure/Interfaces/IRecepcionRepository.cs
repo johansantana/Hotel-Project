840bf5f63968bf2dbe0a127a375766132234086a
Ador <adorsantiago003@gmail.com>
2024-03-15 22:20:28 -0400

    aprueba

--- Hotel.Infrastructure/Interfaces/IRecepcionRepository.cs
+++ Hotel.Infrastructure/Interfaces/IRecepcionRepository.cs
@@ -1,9 +1,13 @@
-using Hotel.Domain.Repository;
-using Hotel.Domain.Entities;
+using Hotel.Domain;
 
 namespace Hotel.Infrastructure.Interfaces
 {
-    public interface IRecepcionRepository : IBaseRepository<Recepcion>
+    public interface IRecepcionRepository
     {
+        void Create(Recepcion recepcion);
+        void Update(Recepcion recepcion);
+        void Remove(Recepcion recepcion);
+        Recepcion GetRecepcion(int id);
+        List<Recepcion> GetRecepcions();
     }
 }
